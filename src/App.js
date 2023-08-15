68f688c19ad23bc2c21217ac5b90a0780314fd7d
Devin Udy <34194818+Devin-The-Dev@users.noreply.github.com>
2023-08-15 11:31:08 -0600

    Added object array. Rendered with a key

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: 
+      [
+        { 
+          id: '12e1231e',
+          name: 'Devin', 
+          num: 0
+        },
+        { 
+          id: '12ed2dacas',
+          name: 'Otis', 
+          num: 0 
+        },
+        { 
+          id: '1231e',
+          name: 'Odie', 
+          num: 0 
+        }
+      ]
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          // 'monster' is our index. This is very much like a for-each loop. map() is the preferrred looping method for React
+          this.state.monsters.map((monster) => {
+            return <h1 key={ monster.id }>{ monster.name } { monster.num }</h1>;
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
